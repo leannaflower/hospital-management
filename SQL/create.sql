993728983a4a14d5bdeb5be616eaf449c89bfbd6
Leanna Fowler <143443662+leannaflower@users.noreply.github.com>
2026-04-19 08:48:10 -0400

    Update: create.sql for hospital management schema to reflect correctly from the actual schemas from milestone 2

--- SQL/create.sql
+++ SQL/create.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS IS_TREATING;
 DROP TABLE IF EXISTS PATIENT;
 DROP TABLE IF EXISTS ROOM;
 DROP TABLE IF EXISTS DOCTOR;
@@ -6,24 +7,23 @@ DROP TABLE IF EXISTS DEPARTMENT;
 
 -- DEPARTMENT
 CREATE TABLE DEPARTMENT (
-    department_id INT PRIMARY KEY,
+    department_id VARCHAR(20) PRIMARY KEY,
     department_name VARCHAR(20),
     department_location INT,
-    beds_total INT,
-    number_of_patients INT
+    beds_total INT
 );
 
 -- DOCTOR
 CREATE TABLE DOCTOR (
-    doctor_id INT PRIMARY KEY,
-    department_id INT,
+    doctor_id VARCHAR(20) PRIMARY KEY,
+    department_id VARCHAR(20),
     first_name VARCHAR(20),
     last_name VARCHAR(20),
-    contact_num INT,
+    contact_num VARCHAR(20),
     shift_start DATETIME,
     shift_end DATETIME,
     is_on_shift BOOLEAN,
-    license_num INT,
+    license_num VARCHAR(20),
     FOREIGN KEY (department_id)
         REFERENCES DEPARTMENT(department_id)
         ON DELETE SET NULL
@@ -32,15 +32,15 @@ CREATE TABLE DOCTOR (
 
 -- NURSE
 CREATE TABLE NURSE (
-    nurse_id INT PRIMARY KEY,
-    department_id INT,
+    nurse_id VARCHAR(20) PRIMARY KEY,
+    department_id VARCHAR(20),
     first_name VARCHAR(20),
     last_name VARCHAR(20),
-    contact_num INT,
+    contact_num VARCHAR(20),
     shift_start DATETIME,
     shift_end DATETIME,
     is_on_shift BOOLEAN,
-    license_num INT,
+    license_num VARCHAR(20),
     FOREIGN KEY (department_id)
         REFERENCES DEPARTMENT(department_id)
         ON DELETE SET NULL
@@ -49,8 +49,8 @@ CREATE TABLE NURSE (
 
 -- ROOM
 CREATE TABLE ROOM (
-    room_num INT PRIMARY KEY,
-    department_id INT NOT NULL,
+    room_num VARCHAR(20) PRIMARY KEY,
+    department_id VARCHAR(20) NOT NULL,
     room_type VARCHAR(10),
     beds_count INT,
     is_filled BOOLEAN,
@@ -64,21 +64,36 @@ CREATE TABLE ROOM (
 -- PATIENT
 CREATE TABLE PATIENT (
     patient_id INT PRIMARY KEY,
-    room_num INT,
+    room_num VARCHAR(20),
     first_name VARCHAR(20),
     last_name VARCHAR(20),
-    contact_info INT,
+    contact_info VARCHAR(20),
     gender VARCHAR(10),
     DOB DATETIME,
     illness VARCHAR(100),
     time_admitted DATETIME,
     status VARCHAR(10),
     insurance VARCHAR(20),
-    insurance_num INT,
+    insurance_num VARCHAR(20),
     height DECIMAL(3,2),
     weight DECIMAL(5,2),
     FOREIGN KEY (room_num)
         REFERENCES ROOM(room_num)
         ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
+-- IS_TREATING
+CREATE TABLE IS_TREATING (
+    doctor_id VARCHAR(20),
+    patient_id INT,
+    PRIMARY KEY (doctor_id, patient_id),
+    FOREIGN KEY (doctor_id)
+        REFERENCES DOCTOR(doctor_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (patient_id)
+        REFERENCES PATIENT(patient_id)
+        ON DELETE CASCADE
         ON UPDATE CASCADE
 );
